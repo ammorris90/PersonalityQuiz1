e22cb3fc477e96979a0777103ac5a5324ec621aa
Andrew Morris <narrowsplitter@gmail.com>
2019-03-05 20:10:39 -0600

    little change

--- PersonalityQuiz/QuestionData.swift
+++ PersonalityQuiz/QuestionData.swift
@@ -28,7 +28,7 @@ enum emperorType : Character {
     var emperorDescription: String {
         switch self {
         case .basil:
-            return "You're Basil the 2nd! In every sense of you are practial and efficient, perhaps to the extreme. When you set your mind to a task you will complete it, no matter what it takes or what gets in the way. While your choices may be controversial at home and abroad, you don't let this stop from making major changes in the world"
+            return "You're Basil the 2nd! You are practial and efficient, perhaps to the extreme. When you set your mind to a task you will complete it, no matter what it takes or what gets in the way. While your choices may be controversial at home and abroad, you don't let this stop from making major changes in the world"
         case .justinian:
             return "You're Justinian the Great! Above all maginficent and bold in ambition. You attracted great friends and exploit their talents. While grand in vision you tend to spend every penny you have and overtend yourself. However your actions and legacy will not be soon forgotten leaving the world a grander, more wonderous place"
         case .komnenos: return "You're Alexious Komnenos! The right person at the right time. A problem solver of the highest calibur and social mastermind of the highest calibur. While you may not shine in conflict, you certainly take the cake when it comes to finding a new solution. Devious and shrewd, you'll get your way, even if its through someone elses moves"
